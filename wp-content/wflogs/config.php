c9a732dbe99e982969dba82fb59ebe4d84956008
dareadel <dare.webdesigns@gmail.com.noreply.github.com>
2026-09-03 22:23:18 -0400

    update

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1789090513;s:7:"authKey";s:64:"+mvE|NIEqPK]G?uRaV~U_6oqK0bP/&rj,lZoY5W*PHVQ^3MJ3thy,U.*UoSV3B8T";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1083;}
